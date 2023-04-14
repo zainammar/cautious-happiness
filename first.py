f18d45ae55b5f2596a29747ae20571913a438430
zainammar <zainammar74@gmail.com>
2023-04-13 20:26:44 -0700

    copy all types of file

---
+++ first.py
@@ -0,0 +1,11 @@
+import shutil
+#import shutil write code and run vscode auto file craeted with name in same folder
+shutil.copyfile('code.txt','Huzefa Tanveer DevOps.txt') 
+shutil.copyfile('code.txt','Shams Ur Rehman Trainer.txt') 
+shutil.copyfile('code.txt','Arsalan Hassan Cloud Architect.txt') 
+shutil.copyfile('code.txt','Zarmeena Anwar DevOps.txt')
+shutil.copyfile('code.txt','Muhammad Muzammil DevOps.txt') 
+shutil.copyfile('code.txt','Moazzam Imran.txt')  
+shutil.copyfile('code.txt','Mujtaba Jawed.txt')  
+shutil.copyfile('code.txt','Hasnain Saeed.txt') 
+shutil.copyfile('code.txt','Faizan Ahmed.txt') 
